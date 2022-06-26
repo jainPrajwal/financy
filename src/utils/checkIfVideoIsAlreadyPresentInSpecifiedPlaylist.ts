c9d57b35611b099acc44e73daafa089b7f3f26c3
unknown <jainprajwal123@gmail.com>
2022-06-26 13:39:03 +0530

    feat: utils function

---
+++ src/utils/checkIfVideoIsAlreadyPresentInSpecifiedPlaylist.ts
@@ -0,0 +1,12 @@
+import { Playlist } from "../constants/playlists.types";
+import { Video } from "../constants/videos.types";
+
+export const checkIfVideoIsAlreadyPresentInSpecifiedPlaylist = (
+  video: Video | undefined,
+  playlist: Playlist
+): Boolean => {
+  const isVideoAlreadyPresent = playlist.videos.some(
+    (videoInPlaylist) => videoInPlaylist._id === video?._id
+  );
+  return isVideoAlreadyPresent ? isVideoAlreadyPresent : false;
+};
