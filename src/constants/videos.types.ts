9700eedb8f93efc13ba575cf5ebc72ff7bacaa47
unknown <jainprajwal123@gmail.com>
2022-06-27 20:57:16 +0530

    fix: thumbnails type in vide

--- src/constants/videos.types.ts
+++ src/constants/videos.types.ts
@@ -1,64 +1,70 @@
 export type ProviderProps = {
-    children: React.ReactNode;
+  children: React.ReactNode;
+};
+type gender = [`male`, `female`, `others`];
+export type loading = `idle` | `success` | `loading` | `error`;
+export const QUERY_LIMIT = 10;
+
+export type ThumbnailObject = {
+ default: Thumbnail;
+ high: Thumbnail;
+ max: Thumbnail;
+ medium: Thumbnail;
+ standard: Thumbnail
+};
+export type Thumbnail = {
+  url: string;
+  width: number;
+  height: number;
+};
+
+export type publisher = {
+  isAPremiumMember: Boolean;
+  _id: string;
+  name: string;
+  email: string;
+  avatar: string;
+  password: string;
+  gender?: gender[number];
+  dateOfBirth?: string;
+};
+export type Video = {
+  category: string;
+  channelId: string;
+  createdAt: Date;
+  description: string;
+  duration: string;
+  isPremium: Boolean;
+  likes: {
+    male: number;
+    female: number;
+    others: number;
   };
-  type gender = [`male`, `female`, `others`];
-  export type loading = `idle` | `success` | `loading` | `error`;
-  export const QUERY_LIMIT = 10;
-  
-  export type Thumbnail = {
-    url: string;
-    width: number;
-    height: number;
+  title: string;
+  updatedAt?: Date;
+  url: string;
+  views: {
+    male: number;
+    female: number;
+    others: number;
   };
-  
-  export type publisher = {
-    isAPremiumMember: Boolean;
-    _id: string;
-    name: string;
-    email: string;
-    avatar: string;
-    password: string;
-    gender?: gender[number];
-    dateOfBirth?: string;
-  };
-  export type Video = {
-    category: string;
-    channelId: string;
-    createdAt: Date;
-    description: string;
-    duration: string;
-    isPremium: Boolean;
-    likes: {
-      male: number;
-      female: number;
-      others: number;
-    };
-    title: string;
-    updatedAt?: Date;
-    url: string;
-    views: {
-      male: number;
-      female: number;
-      others: number;
-    };
-    _id: string;
-    __v?: number;
-    publisher: publisher;
-    thumbnails: Array<Thumbnail>;
-  };
-  
-  export type VideosInitialState = {
-    videos: Array<Video>;
-    loading: `idle` | `loading` | `error` | `success`;
-    sortBy: string | null;
-    categories: [`stockmarket`, `scams`];
-    searchQuery: string;
-    selectedCategory: string;
-    currentPageNumber: number;
-  };
-  
-  export type UserUploadedVideo = {
-    url: string | null;
-    category: string | null;
-  };
-  
+  _id: string;
+  __v?: number;
+  publisher: publisher;
+  thumbnails: Array<ThumbnailObject>;
+};
+
+export type VideosInitialState = {
+  videos: Array<Video>;
+  loading: `idle` | `loading` | `error` | `success`;
+  sortBy: string | null;
+  categories: [`stockmarket`, `scams`];
+  searchQuery: string;
+  selectedCategory: string;
+  currentPageNumber: number;
+};
+
+export type UserUploadedVideo = {
+  url: string | null;
+  category: string | null;
+};
