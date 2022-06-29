4d8c6cd24c948eb04980ffe8ab0a64f4da04e51b
unknown <jainprajwal123@gmail.com>
2022-06-29 23:11:42 +0530

    feat: signup service

---
+++ src/services/auth/signupService.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosResponse } from "axios";
+import { BASE_API } from "../../constants/api";
+import { signupUserCredentials } from "../../constants/auth.types";
+import { getErrorMessage } from "../../utils/getErrorMessage";
+export const signupService = async ({
+  userSignUpCredentials,
+}: {
+  userSignUpCredentials: signupUserCredentials;
+}): Promise<AxiosResponse> => {
+  try {
+    const response = await axios.post(`${BASE_API}/signup`, {
+      user: userSignUpCredentials,
+    });
+    return response;
+  } catch (error) {
+    console.error(`somehting went wrong while signing up the user `, error);
+    throw new Error(getErrorMessage(error));
+  }
+};
