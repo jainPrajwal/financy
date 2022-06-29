1c269072be5a73f3e324799f05be806b2f07e728
unknown <jainprajwal123@gmail.com>
2022-06-29 23:09:50 +0530

    refactor: header element to navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { MdMenu } from "react-icons/md";
+import { default as common } from "../../common/common.module.css";
+import { useProfile } from "../../hooks/useProfile";
+import { Avatar } from "kaali-ui"
+
+export const Navbar = ({ setSidebar }: { setSidebar: React.Dispatch<React.SetStateAction<boolean>> }) => {
+
+    const {
+        navbar,
+        publisherAvatar,
+        wrapperLogo,
+        hamburgerMenu,
+
+    } = common;
+    const { userProfile } = useProfile();
+    return <header className={`${navbar} pr-lg`}>
+        <div
+            className={`${hamburgerMenu} text-white`}
+            role="button"
+            onClick={() => setSidebar(true)}
+        >
+            <MdMenu size={28} />
+        </div>
+        <div className={`${wrapperLogo}`}>
+            <img
+                src="https://res.cloudinary.com/dmk11fqw8/image/upload/v1653841636/Tube_Stox-removebg-preview_ezjluc_qkz2zk.png"
+                alt="logo"
+                width={`100%`}
+                height={`100%`}
+            />
+        </div>
+        <div className="ml-auto">
+            <div className={`${publisherAvatar}`}>
+                <Avatar
+                    size={`sm`}
+                    imageUrl={userProfile?.gender === `male` ? `https://res.cloudinary.com/dmk11fqw8/image/upload/v1653926221/man_6_ewkhrj.png` : `https://res.cloudinary.com/dmk11fqw8/image/upload/v1656501210/woman_1_jotf2w.png`}
+                />
+            </div>
+        </div>
+    </header>
+}
