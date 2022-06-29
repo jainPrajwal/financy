8b3f1e2cb109e89ee2f4d13bc05826fdc3a8646e
unknown <jainprajwal123@gmail.com>
2022-06-29 23:01:12 +0530

    feat: liked page

--- src/pages/Liked/Liked.tsx
+++ src/pages/Liked/Liked.tsx
@@ -1,28 +1,67 @@
 
+import { useState } from "react";
+import { MdMenu } from "react-icons/md";
+import { default as common } from "../../common/common.module.css";
 
-import { LikedVideoCard } from "../../components/LikedVideoCard";
+import { Video } from "../../constants/videos.types";
 import { usePlaylists } from "../../hooks/usePlaylists";
-
+import { default as historyStyles } from "../History/History.module.css";
+import { Avatar, Loader } from "kaali-ui";
+import { MobileSidebar } from "../../components/MobileSidebar/MobileSidebar";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
+import { PlaylistsVideoCard } from "../../components/PlaylistsCard/PlaylistsVideoCard";
+import { Navbar } from "../../components/Navbar/Navbar";
 export const Liked = () => {
-  const { playlistsState } = usePlaylists()
-  const { likedVideosData } = playlistsState;
+  const {
+    navbar,
+    publisherAvatar,
+    wrapperLogo,
+    hamburgerMenu,
+    btnTrash
+  } = common;
+  const {
+    historyContainer,
+    headerContainer,
+    mainContainer,
+    publisherName,
+    cardContainer,
+    card,
+    cardWrapper,
+    cardImageWrapper,
 
-  if (likedVideosData.likedVideos.videos.length > 0) {
-    return (
-      <>
-        <div className="header header-secondary p-1">
-          {likedVideosData.likedVideos.videos.map((video, index) => {
-            
-            return <LikedVideoCard video={video} key={video._id} index={index} />
-          })}
+    cardImage,
+    cardTitle,
+    iconDelete
+  } = historyStyles;
+  const [sidebar, setSidebar] = useState(false);
+  const { playlistsState } = usePlaylists();
+  return (
+    <>
+  <Navbar setSidebar={setSidebar} />
+      <MobileSidebar status={{ sidebar, setSidebar }} />
+      <div className={`${historyContainer}`}>
+        <div
+          className={`${headerContainer} header header-secondary text-white`}
+        >
+          Liked
         </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div className="header header-secondary">No Liked Videos :( </div>
-      </>
-    );
-  }
+
+        <Sidebar />
+        <div className={`${mainContainer}`}>
+          <div className={`${cardContainer}`}>
+
+
+            {
+              playlistsState.likedVideosData.likedVideos.videos.map((video: Video) => {
+                return <PlaylistsVideoCard key={video._id} video={video} 
+                playlistId={null}
+                />
+              })
+            }
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
