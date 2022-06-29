531c7ae5cca09aea7b08bfee2980b652695da15c
unknown <jainprajwal123@gmail.com>
2022-06-29 22:58:23 +0530

    feat: login ans signup auth

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -1,65 +1,76 @@
 import {
-    createContext,
-    useCallback,
-    useEffect,
-    useReducer,
-    useState,
-  } from "react";
-  import { useLocation, useNavigate } from "react-router-dom";
-  import { AuthState, UserLoginCredentials } from "../constants/auth.types";
-  import { loading, ProviderProps } from "../constants/videos.types";
-  import { useAsync } from "../hooks/useAxios";
-  import { useLocalStorage } from "../hooks/useLocalStorage";
-  import { AuthReducer } from "../reducers/AuthReducer";
-  import {
-    loginService,
-    setupAuthExceptionHandler,
-    setupAuthHeaderForServiceCalls,
-  } from "../services/auth/loginService";
-  
-  export const AuthContext = createContext<{
-    authState: AuthState;
-    loginUserWithCredentials: (
-      userLoginCredentials: UserLoginCredentials
-    ) => void;
-    status: loading;
-  }>({
-    authState: { loggedInUser: null, token: null },
-    loginUserWithCredentials: (userLoginCredentials: UserLoginCredentials) => { },
-    status: `idle`,
-  });
-  
-  export const initialAuthState: AuthState = {
-    loggedInUser: null,
-    token: null,
-  };
-  
-  export const AuthProvider = ({ children }: ProviderProps) => {
-  
-    const [authState, authDispatch] = useReducer(AuthReducer, initialAuthState);
-    const [email, setEmail] = useState<string | null>(null);
-    const navigate = useNavigate();
-    const { state } = useLocation();
-  
-    const { execute, status, response } = useAsync(loginService, false, null);
-  
-    const [localStorageItem, setLocalStorageItem] = useLocalStorage<AuthState>(
-      `token`,
-      {
-        loggedInUser: null,
-        token: null,
-      }
-    );
-  
-    const logout = useCallback(() => {
-      localStorage.clear();
+  createContext,
+  useCallback,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { AuthState, signupUserCredentials, UserLoginCredentials } from "../constants/auth.types";
+import { loading, ProviderProps } from "../constants/videos.types";
+import { useAsync } from "../hooks/useAxios";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { AuthReducer } from "../reducers/AuthReducer";
+import {
+  loginService,
+  setupAuthExceptionHandler,
+  setupAuthHeaderForServiceCalls,
+} from "../services/auth/loginService";
+import { signupService } from "../services/auth/signupService";
+
+export const AuthContext = createContext<{
+  authState: AuthState;
+  loginUserWithCredentials: (
+    userLoginCredentials: UserLoginCredentials
+  ) => void;
+  signUpUserWithCredentials: (userSignUpCredentials: signupUserCredentials) => void;
+  status: loading;
+}>({
+  authState: { loggedInUser: null, token: null },
+  loginUserWithCredentials: (userLoginCredentials: UserLoginCredentials) => { },
+  signUpUserWithCredentials: (userSignUpCredentials: signupUserCredentials) => { },
+  status: `idle`,
+});
+
+export const initialAuthState: AuthState = {
+  loggedInUser: null,
+  token: null,
+};
+
+
+export const AuthProvider = ({ children }: ProviderProps) => {
+
+  const [authState, authDispatch] = useReducer(AuthReducer, initialAuthState);
+  const [email, setEmail] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+  const { execute, status, response } = useAsync(loginService, false, null);
+  const { execute: executeSignup, response: signupResponse, status: signupStatus } = useAsync(signupService, false, null)
+
+  const [localStorageItem, setLocalStorageItem] = useLocalStorage<AuthState>(
+    `token`,
+    {
+      loggedInUser: null,
+      token: null,
+    }
+  );
+
+  const logout = useCallback(() => {
+    localStorage.clear();
+    authDispatch({
+      type: `LOGOUT`,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (localStorageItem.token) {
       authDispatch({
-        type: `LOGOUT`,
+        type: `LOGIN_USER`,
+        payload: localStorageItem,
       });
-    }, []);
-  
-    useEffect(() => {
-  
+      (state as { from: string })?.from && navigate(`${(state as { from: string }).from}`)
+    } else {
       if (status === `success`) {
         const {
           data: { token },
@@ -68,58 +79,84 @@ import {
           loggedInUser: email,
           token,
         };
-  
+
         authDispatch({
           type: `LOGIN_USER`,
           payload: authState,
         });
         setupAuthHeaderForServiceCalls(authState.token);
         setLocalStorageItem(authState);
-        
-        (state as { from: string })?.from && navigate(`${(state as { from: string }).from}`) 
-      } else {
-        if (localStorageItem.token) {
-          authDispatch({
-            type: `LOGIN_USER`,
-            payload: localStorageItem,
-          });
-          (state as { from: string })?.from && navigate(`${(state as { from: string }).from}`) 
-        }
+
+        (state as { from: string })?.from ? navigate(`${(state as { from: string }).from}`) : navigate(`/`)
       }
-    }, [status]);
-  
-    useEffect(() => {
-      if (localStorageItem.token) {
-  
-        setupAuthHeaderForServiceCalls(localStorageItem.token);
-      } else if (authState.token) {
-  
-  
+    }
+
+  }, [status]);
+
+  useEffect(() => {
+    if (localStorageItem.token) {
+      authDispatch({
+        type: `LOGIN_USER`,
+        payload: localStorageItem,
+      });
+      (state as { from: string })?.from ? navigate(`${(state as { from: string }).from}`) : navigate(`/`)
+    } else {
+      if (signupStatus === `success`) {
+        const {
+          data: { token, user },
+        } = signupResponse;
+        const authState = {
+          loggedInUser: user?.email,
+          token,
+        };
+
+        authDispatch({
+          type: `LOGIN_USER`,
+          payload: authState,
+        });
         setupAuthHeaderForServiceCalls(authState.token);
+        setLocalStorageItem(authState);
+
+        (state as { from: string })?.from ? navigate(`${(state as { from: string }).from}`) : navigate(`/`)
       }
-    }, [authState.token, localStorageItem]);
-  
-    const loginUserWithCredentials = useCallback(
-      (userLoginCredentials: UserLoginCredentials) => {
-  
-        setEmail(userLoginCredentials.email);
-  
-  
-        execute(userLoginCredentials);
-      },
-      [execute]
-    );
-  
-    const signupUserWithCredentials = () => { };
-  
-    setupAuthExceptionHandler({ navigate, logout });
-  
-    return (
-      <AuthContext.Provider
-        value={{ authState, loginUserWithCredentials, status }}
-      >
-        {children}
-      </AuthContext.Provider>
-    );
-  };
-  
+    }
+
+  }, [signupStatus]);
+
+  useEffect(() => {
+    if (localStorageItem.token) {
+
+      setupAuthHeaderForServiceCalls(localStorageItem.token);
+    } else if (authState.token) {
+
+
+      setupAuthHeaderForServiceCalls(authState.token);
+    }
+  }, [authState.token, localStorageItem]);
+
+  const loginUserWithCredentials = useCallback(
+    (userLoginCredentials: UserLoginCredentials) => {
+
+      setEmail(userLoginCredentials.email);
+
+
+      execute(userLoginCredentials);
+    },
+    [execute]
+  );
+
+  const signUpUserWithCredentials = useCallback((userSignUpCredentials: signupUserCredentials) => {
+    console.log(`user`, userSignUpCredentials)
+    executeSignup({ userSignUpCredentials })
+  }, [executeSignup])
+
+  setupAuthExceptionHandler({ navigate, logout });
+
+  return (
+    <AuthContext.Provider
+      value={{ authState, loginUserWithCredentials, status, signUpUserWithCredentials }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
