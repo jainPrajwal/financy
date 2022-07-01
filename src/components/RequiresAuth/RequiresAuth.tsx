4f66ba2c35508612b6039b64d38b99b34b3c2ff9
unknown <jainprajwal123@gmail.com>
2022-07-01 13:29:51 +0530

    fix: token from local storage

--- src/components/RequiresAuth/RequiresAuth.tsx
+++ src/components/RequiresAuth/RequiresAuth.tsx
@@ -1,10 +1,14 @@
+import { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { ProviderProps } from "../../constants/videos.types";
 import { useAuth } from "../../hooks/useAuth"
 
 export const RequiresAuth = ({ children }: ProviderProps) => {
     const { authState } = useAuth();
+    const localToken = localStorage.getItem(`token`);
+    const parsedToken = localToken && (JSON.parse(localToken));
+    const [token] = useState(authState.token || (parsedToken?.token))
     const location = useLocation();
-    return authState.token ? <>{children}</> : <Navigate to="/login" replace state={{ from: location.pathname }} />
+    return token ? <>{children}</> : <Navigate to="/login" replace state={{ from: location.pathname }} />
 
 }
